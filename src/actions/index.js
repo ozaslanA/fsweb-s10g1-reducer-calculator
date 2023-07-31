37981451dfe4c0ad17f9d2770c73cd6295be1b5a
ozaslanA <118729265+ozaslanA@users.noreply.github.com>
2023-07-31 18:00:58 +0300

    tamamlandı

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,33 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMO_TOTAL = "MEMO_TOTAL";
+
+export const TOTAL_MEMO = "TOTAL_MEMO";
+export const CLEAR_MEMO = "CLEAR_MEMO";
 
 export const addOne = () => {
-  return ({ type: ADD_ONE });
-}
+  return { type: ADD_ONE };
+};
 
 export const applyNumber = (number) => {
-  return ({ type: APPLY_NUMBER, payload: number });
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
+export const changeOperation = (operator) => {
+  return { type: CHANGE_OPERATION, payload: operator };
+};
+
+export const clearDisplay = () => {
+  return { type: CLEAR_DISPLAY };
+};
+export const memoToTotal = () => {
+  return { type: MEMO_TOTAL };
+};
+export const totalToMemo = () => {
+  return { type: TOTAL_MEMO };
+};
+
+export const clearMemo = () => {
+  return { type: CLEAR_MEMO };
+};
